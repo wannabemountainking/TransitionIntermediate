943aaa0b8b798b60dfc94616181e4576ecc9c870
wannabemountainking <kdy0096@gmail.com>
2026-08-04 12:07:19 +0900

    Basic Header

--- TransitionIntermediate/TransitionBasic/BasicTransitionOption.swift
+++ TransitionIntermediate/TransitionBasic/BasicTransitionOption.swift
@@ -7,9 +7,34 @@
 
 import SwiftUI
 
+
+struct FavoriteItem: Identifiable {
+	static var index: Int = 0
+	
+	let id: UUID = UUID()
+	let name: String
+	
+	init() {
+		Self.index += 1
+		self.name = "아이템 \(Self.index)"
+	}
+}
+
 struct BasicTransitionOption: View {
+	@State private var items: [FavoriteItem] = []
+	@State private var itemSoonToBeAdded: FavoriteItem = FavoriteItem()
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		ScrollView {
+			Button {
+				// Action
+				withAnimation(.easeInOut(duration: 1)) {
+					items.append(itemSoonToBeAdded)
+				}
+			} label: {
+				Text("<#placeholder#>")
+			}
+		}
     }
 }
 
