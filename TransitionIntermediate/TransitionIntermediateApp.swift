5214f9a1de210eef6167fb28af24673b8586d122
wannabemountainking <kdy0096@gmail.com>
2026-08-04 11:21:34 +0900

    Initial Commit

---
+++ TransitionIntermediate/TransitionIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionIntermediateApp.swift
+//  TransitionIntermediate
+//
+//  Created by YoonieMac on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TransitionIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
