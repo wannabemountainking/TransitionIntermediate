fd2b4d2682522caa4fbc0b8f880d62476746a376
wannabemountainking <kdy0096@gmail.com>
2026-08-04 11:28:00 +0900

    initialCommit

---
+++ TransitionIntermediate/TransitionBasic/BasicTransitionOption.swift
@@ -0,0 +1,18 @@
+//
+//  TransitionBasic.swift
+//  TransitionIntermediate
+//
+//  Created by YoonieMac on 8/4/26.
+//
+
+import SwiftUI
+
+struct BasicTransitionOption: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BasicTransitionOption()
+}
